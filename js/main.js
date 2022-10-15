e4a81963b2791ce9449fe04cbca75f537ccf5e93
Ccccadyrov <msheplyakovq@gmail.com>
2022-10-15 14:30:47 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 function randomGenerator(minNumber, maxNumber) {
   if (minNumber >= 0 && maxNumber > 1 && maxNumber > minNumber) {
     return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
-    }
-  return NaN;  
+  }
+  return NaN;
 }
 randomGenerator(1, 200);
 
@@ -18,22 +18,21 @@ commentCheck(100, 140)
 let descriptionContainer = ["Black cat photo", "Orange cat photo", "White cat photo"];
 //ниже - получаем случайный комментарий из массива строк
 function getComment() {
-  return descriptionContainer[randomGenerator(0,descriptionContainer.length-1)]
+  return descriptionContainer[randomGenerator(0, descriptionContainer.length - 1)]
 }
 //ниже функция для генерации объекта с (массив,количество_элементов)
-function getArray (array,count) {
-  for (let j=0; j<count; j++) {
-    array[j]={
-      id:j+1, 
-      likes:randomGenerator(15,200),
-      comments:randomGenerator(0,200),
-      url:`photos/${j+1}.jpg`,
-      description:getComment()
-      };
-    }
+function getArray(array, count) {
+  for (let j = 0; j < count; j++) {
+    array[j] = {
+      id: j + 1,
+      likes: randomGenerator(15, 200),
+      comments: randomGenerator(0, 200),
+      url: `photos/${j + 1}.jpg`,
+      description: getComment()
+    };
+  }
   return array;
 }
 
 let test = [];
-getArray (test,25);
-console.log(test)
+getArray(test, 25);
