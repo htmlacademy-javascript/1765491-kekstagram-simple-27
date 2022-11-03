e78d4311ed1bfac3d535655114b17910da78861e
Keks <keks@htmlacademy.ru>
2022-11-03 12:30:19 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/length-check.js
@@ -0,0 +1,5 @@
+//функция ниже проверяет длину строки
+function commentCheck(commentLenght, maxCommentLenght) {
+  return commentLenght <= maxCommentLenght;
+}
+export {commentCheck};
---
+++ 5/js/main.js
@@ -0,0 +1,35 @@
+import {getArray} from './get-photo-properties.js';
+import {miniatureGenerator} from './miniature-generator.js';
+const test = [];
+getArray(test, 19);
+miniatureGenerator(test);
+
+
+// const photoContainer = document.querySelector('.pictures')
+// const Field = document.querySelector('#picture').content;
+// const pictureField = Field.querySelector('.picture');
+// console.log (pictureField);
+
+// let miniatureGenerator = function () {
+//   for (let i=0; i<test.length; i++) {
+//     let miniPic = pictureField.cloneNode(true);
+//     let url = miniPic.querySelector('.picture__img');
+//     let comments = miniPic.querySelector('.picture__comments');
+//     let likes = miniPic.querySelector('.picture__likes');
+//     url.src = test[i].url;
+//     url.alt = test[i].description;
+//     comments.insertAdjacentHTML('beforeend', test[i].comments);
+//     likes.insertAdjacentHTML('beforeend', test[i].likes);
+//     console.log(miniPic);
+//     photoContainer.appendChild(miniPic);
+//   }
+// };
+// <template id="picture">
+//     <a href="#" class="picture">
+//       <img class="picture__img" src="" width="182" height="182" alt="Случайная фотография">
+//       <p class="picture__info">
+//         <span class="picture__comments"></span>
+//         <span class="picture__likes"></span>
+//       </p>
+//     </a>
+//   </template>
