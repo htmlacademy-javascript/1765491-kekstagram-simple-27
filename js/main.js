0c3ffe5cc70035be9fcb2d46b9458e9552d9f238
Ccccadyrov <msheplyakovq@gmail.com>
2022-11-03 19:27:27 +1000

    123

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import {getArray} from './get-photo-properties.js';
 import {miniatureGenerator} from './miniature-generator.js';
 const test = [];
-getArray(test, 15);
+getArray(test, 19);
 miniatureGenerator(test);
 
 
